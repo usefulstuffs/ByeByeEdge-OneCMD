36afc33634ebac4663f4f278067025b399b98136
Useful Stuffs <157041205+usefulstuffs@users.noreply.github.com>
2024-08-07 10:06:11 +0200

    Update UninstallEdge.cmd

--- UninstallEdge.cmd
+++ UninstallEdge.cmd
@@ -82,7 +82,7 @@ del /f /q "%ProgramData%\Microsoft\Windows\Start Menu\Programs\Microsoft Edge.ln
 del /f /q "%appdata%\Microsoft\Windows\Start Menu\Programs\Microsoft Edge.lnk" > nul
 del /f /q "%appdata%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Microsoft Edge.lnk" > nul
 reg add "HKLM\Software\Microsoft\EdgeUpdate" /v "DoNotUpdateToEdgeWithChromium" /t REG_DWORD /d 1 /f > nul
-for /f %f in ('dir /s /b %SystemRoot%\System32\Tasks\*MicrosoftEdge*') do ('takeown /f "%f" && icacls "%f" /grant everyone:F && del /f /q "%f"')
+for /f %f in ('dir /s /b %SystemRoot%\System32\Tasks\*MicrosoftEdge*') do ('takeown /f "%f" & icacls "%f" /grant everyone:F & del /f /q "%f"')
 for /f "delims=" %%a in ('powershell "(New-Object System.Security.Principal.NTAccount($env:USERNAME)).Translate([System.Security.Principal.SecurityIdentifier]).Value"') do set "USER_SID=%%a"
 for /f "delims=" %%a in ('powershell -NoProfile -Command "Get-AppxPackage -AllUsers ^| Where-Object { $_.PackageFullName -like '*microsoftedge*' } ^| Select-Object -ExpandProperty PackageFullName"') do (
     if not "%%a"=="" (
@@ -94,8 +94,8 @@ for /f "delims=" %%a in ('powershell -NoProfile -Command "Get-AppxPackage -AllUs
         powershell -Command "Remove-AppxPackage -Package '!APP!' -AllUsers" 2>nul
     )
 )
-for /f %f in ('dir /s /b %SystemRoot%\SystemApps\Microsoft.MicrosoftEdge*') do ('takeown /f "%f" && icacls "%f" /grant everyone:F && del /f /q "%f"')
-for /f %f in ('dir /s /b %SystemRoot%\System32\MicrosoftEdge*.exe') do ('takeown /f "%f" && icacls "%f" /grant everyone:F && del /f /q "%f"')
+for /f %f in ('dir /s /b %SystemRoot%\SystemApps\Microsoft.MicrosoftEdge*') do ('takeown /f "%f" & icacls "%f" /grant everyone:F & del /f /q "%f"')
+for /f %f in ('dir /s /b %SystemRoot%\System32\MicrosoftEdge*.exe') do ('takeown /f "%f" & icacls "%f" /grant everyone:F & del /f /q "%f"')
 echo Microsoft Edge should be now uninstalled.
 echo Please reboot Windows.
 pause
@@ -132,7 +132,7 @@ del /f /q "%appdata%\Microsoft\Windows\Start Menu\Programs\Microsoft Edge.lnk" >
 del /f /q "%appdata%\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Microsoft Edge.lnk" > nul
 reg add "HKLM\Software\Microsoft\EdgeUpdate" /v "DoNotUpdateToEdgeWithChromium" /t REG_DWORD /d 1 /f > nul
 reg add "HKLM\Software\WOW6432Node\Microsoft\EdgeUpdate" /v "DoNotUpdateToEdgeWithChromium" /t REG_DWORD /d 1 /f > nul
-for /f %f in ('dir /s /b %SystemRoot%\System32\Tasks\*MicrosoftEdge*') do ('takeown /f "%f" && icacls "%f" /grant everyone:F && del /f /q "%f"')
+for /f %f in ('dir /s /b %SystemRoot%\System32\Tasks\*MicrosoftEdge*') do ('takeown /f "%f" & icacls "%f" /grant everyone:F & del /f /q "%f"')
 for /f "delims=" %%a in ('powershell "(New-Object System.Security.Principal.NTAccount($env:USERNAME)).Translate([System.Security.Principal.SecurityIdentifier]).Value"') do set "USER_SID=%%a"
 for /f "delims=" %%a in ('powershell -NoProfile -Command "Get-AppxPackage -AllUsers ^| Where-Object { $_.PackageFullName -like '*microsoftedge*' } ^| Select-Object -ExpandProperty PackageFullName"') do (
     if not "%%a"=="" (
@@ -144,9 +144,9 @@ for /f "delims=" %%a in ('powershell -NoProfile -Command "Get-AppxPackage -AllUs
         powershell -Command "Remove-AppxPackage -Package '!APP!' -AllUsers" 2>nul
     )
 )
-for /f %f in ('dir /s /b %SystemRoot%\SystemApps\Microsoft.MicrosoftEdge*') do ('takeown /f "%f" && icacls "%f" /grant everyone:F && del /f /q "%f"')
-for /f %f in ('dir /s /b %SystemRoot%\System32\MicrosoftEdge*.exe') do ('takeown /f "%f" && icacls "%f" /grant everyone:F && del /f /q "%f"')
-for /f %f in ('dir /s /b %SystemRoot%\SysWOW64\MicrosoftEdge*.exe') do ('takeown /f "%f" && icacls "%f" /grant everyone:F && del /f /q "%f"')
+for /f %f in ('dir /s /b %SystemRoot%\SystemApps\Microsoft.MicrosoftEdge*') do ('takeown /f "%f" & icacls "%f" /grant everyone:F & del /f /q "%f"')
+for /f %f in ('dir /s /b %SystemRoot%\System32\MicrosoftEdge*.exe') do ('takeown /f "%f" & icacls "%f" /grant everyone:F & del /f /q "%f"')
+for /f %f in ('dir /s /b %SystemRoot%\SysWOW64\MicrosoftEdge*.exe') do ('takeown /f "%f" & icacls "%f" /grant everyone:F & del /f /q "%f"')
 echo Microsoft Edge should be now uninstalled.
 echo Please reboot Windows.
 pause
